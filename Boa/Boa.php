55551c2c8b143cfcbf036f1efdafc87010c38f66
lewmilburn <lewiscmilburn@gmail.com>
2022-02-19 12:36:44 +0000

    Added config values for hashing.

--- Boa/Boa.php
+++ Boa/Boa.php
@@ -11,13 +11,18 @@ class App {
 
     public function config(): array {
         return array (
+            // Database Settings
             'db_hostname' => '',
             'db_username' => '',
             'db_password' => '',
             'db_database' => '',
             'db_port' => NULL,
             'db_socket' => NULL,
-            'db_security' => true
+            'db_security' => true,
+            // Security Settings
+            'password_hash' => 'PASSWORD_DEFAULT',
+            'ip_hash' => 'sha3-512',
+            'other_hash' => 'sha3-512'
         );
     }
 }
--- Boa/Security/Encryption.php
+++ Boa/Security/Encryption.php
@@ -6,11 +6,25 @@ class Encryption extends Security
 {
     public function hash_ip($ip): string
     {
-        return hash('sha3-512', $ip);
+        global $settings;
+        return hash($settings['ip_hash'], $ip);
     }
 
     public function hash($data): string
     {
-        return hash('sha3-512', $data);
+        global $settings;
+        return hash($settings['other_hash'], $data);
+    }
+
+    public function hash_password($password): string
+    {
+        global $settings;
+        return password_hash($password,$settings['password_hash']);
+    }
+
+    public function verify_password($password): bool
+    {
+        global $settings;
+        return password_verify($password,$settings['password_hash']);
     }
 }
