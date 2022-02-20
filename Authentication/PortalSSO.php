69459070022425b7c0df9d46eba3c546c1bb6ff6
lewmilburn <lewiscmilburn@gmail.com>
2022-02-20 01:19:03 +0000

    Added parent construct

--- Authentication/PortalSSO.php
+++ Authentication/PortalSSO.php
@@ -7,6 +7,7 @@ use Boa\App;
 class PortalSSO extends App
 {
     public function __construct() {
+        parent::__construct();
         $Boa = new App();
         $settings = $Boa->Settings();
     }
