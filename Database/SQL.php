b769b9cdc4b3064810a81d6869cf65d70e37fa96
lewmilburn <lewiscmilburn@gmail.com>
2022-02-20 18:27:32 +0000

    Added select, insert, update, delete functions.

--- Database/SQL.php
+++ Database/SQL.php
@@ -58,6 +58,55 @@ class SQL extends App
 
     }
 
+    public function Select($select, $table, $mode) {
+        if ($this->settings['database_security']) {
+            $select = $this->Escape($select);
+            $table = $this->Escape($table);
+        }
+
+        $result = $this->query("SELECT '$select' FROM '$table';");
+
+        return match ($mode) {
+            'NONE' => $result,
+            'ALL' => $result->fetch_all(),
+            'ALL:ASSOC' => $result->fetch_all(MYSQLI_ASSOC),
+            'ALL:NUMERIC' => $result->fetch_all(MYSQLI_NUM),
+            'ALL:BOTH' => $result->fetch_all(MYSQLI_BOTH),
+            'ASSOC' => $result->fetch_assoc(),
+            'ARRAY' => $result->fetch_array(),
+            'OBJECT' => $result->fetch_object(),
+            'NUMROWS' => $result->num_rows,
+            default => '$mode defined incorrectly.',
+        };
+    }
+
+    public function Insert($into, $values) {
+        if ($this->settings['database_security']) {
+            $into = $this->Escape($into);
+            $values = $this->Escape($values);
+        }
+
+        return $this->query("INSERT INTO '$into' VALUES ($values);");
+    }
+
+    public function Update($table, $set, $where) {
+        if ($this->settings['database_security']) {
+            $table = $this->Escape($table);
+            $where = $this->Escape($where);
+        }
+
+        return $this->query("UPDATE '$table' SET $set WHERE $where;");
+    }
+
+    public function Delete($table, $where) {
+        if ($this->settings['database_security']) {
+            $table = $this->Escape($table);
+            $where = $this->Escape($where);
+        }
+
+        return $this->query("DELETE FROM '$table' WHERE $where;");
+    }
+
     public function Escape($string): String {
         return $this->connect->escape_string($string);
     }
