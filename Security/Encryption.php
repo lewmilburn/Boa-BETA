148011bc4bc54114b1ae4130c48a4d720daf4e73
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2022-07-17 14:50:30 +0100

    Update Encryption.php
    Fixed bugs

--- Security/Encryption.php
+++ Security/Encryption.php
@@ -18,8 +18,7 @@ class Encryption extends App
     {
         parent::__construct();
 
-        global $settings;
-        $settings = array(
+        $this->settings = array(
             'password_hash' => 'PASSWORD_DEFAULT',
             'ip_hash' => 'sha3-512',
             'other_hash' => 'sha3-512',
@@ -28,25 +27,21 @@ class Encryption extends App
 
     public function hash_ip($ip): string
     {
-        global $settings;
-        return hash($settings['ip_hash'], $ip);
+        return hash($this->settings['ip_hash'], $ip);
     }
 
     public function hash($data): string
     {
-        global $settings;
-        return hash($settings['other_hash'], $data);
+        return hash($this->settings['other_hash'], $data);
     }
 
     public function hash_password($password): string
     {
-        global $settings;
-        return password_hash($password,$settings['password_hash']);
+        return password_hash($password,$this->settings['password_hash']);
     }
 
     public function verify_password($password): bool
     {
-        global $settings;
-        return password_verify($password,$settings['password_hash']);
+        return password_verify($password,$this->settings['password_hash']);
     }
 }
